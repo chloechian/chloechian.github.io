4c826ae07dcd07518f67db6a89564902f74420c5
chloechian <163489615+chloechian@users.noreply.github.com>
2024-06-14 09:14:12 +0800

    Add files via upload

---
+++ common.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.getElementById('myVideo');
+  
+    video.addEventListener('ended', function () {
+      this.currentTime = 0;
+      this.play();
+    }, false);
+  });
+  
